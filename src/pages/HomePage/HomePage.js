3161e25f9d964bb5a631384e3a607252edb765d9
shivanshpatel13 <shivanshp13@gmail.com>
2023-04-24 21:09:59 -0400

    strength page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,9 +4,9 @@ import "./HomePage.css"
 export default function HomePage() {
     return (
         <div id="Home-page-body">
-            <div id="Home-page-round-background-decoration"></div>
-            <div id="Home-page-bottom-top-banner-background-decoration"></div>
-            <div id="Home-page-bottom-bot-banner-background-decoration"></div>
+            <div className="default-background-round round-background-decoration"></div>
+            <div className="default-background-top-banner bottom-top-banner-background-decoration"></div>
+            <div className="default-background-bottom-banner bottom-bot-banner-background-decoration"></div>
             <div id="Home-island">
                 Give us feedback!
             </div>
