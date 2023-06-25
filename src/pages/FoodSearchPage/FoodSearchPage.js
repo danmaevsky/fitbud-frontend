07398a2b55ffa0834ace04f311b069833170df66
danmaevsky <dan.mayevskiy@gmail.com>
2023-06-25 11:37:22 -0400

    Minor enhancement related to automatically switching search modes when fetching foods

--- src/pages/FoodSearchPage/FoodSearchPage.js
+++ src/pages/FoodSearchPage/FoodSearchPage.js
@@ -73,7 +73,6 @@ export default function FoodSearchPage() {
                 })
                 .catch((err) => {
                     setSearchType("all");
-
                     setSearchResults([]);
                     console.log(err);
                 });
